976ee12c6116a94c126d671650581f775f4968ea
PedroAvila <minimainor_austin@hotmail.com>
2021-05-08 07:21:11 -0500

    Rutas de nuestra aplicación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRouterModule } from './app-router.module';
 
 import { AppComponent } from './app.component';
 
@@ -14,7 +15,7 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     SharedModule,
-    
+    AppRouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
