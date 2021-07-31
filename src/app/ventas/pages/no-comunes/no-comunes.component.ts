9a062d4d729137959a9664888cb618981b536ba1
PedroAvila <minimainor_austin@hotmail.com>
2021-07-31 16:56:17 -0500

    Pipes slice

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-no-comunes',
@@ -6,11 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class NoComunesComponent implements OnInit {
+export class NoComunesComponent  {
 
-  constructor() { }
+  // i18nSelect
+  nombre: string = 'Fernando';
+  genero: string = 'masculino';
 
-  ngOnInit(): void {
+  invitacionMapa = {
+    'masculino': 'invitarlo',
+    'femenino': 'invitarla'
+  }
+
+  // i18nPlural
+  clientes: string[] = ['Maria', 'Pedro', 'Hernando', 'Eduardo', 'Fernando']
+  clientesMapa = {
+    '=0': 'no tenemos ningun cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    '=2': 'tenemos 2 clientes esperando.',
+    'other': 'tenemos # clientes esperando.'
+  }
+
+  cambiarCliente() {
+    this.nombre = 'Melissa';
+    this.genero = 'femenino';
+  }
+
+  borrarCliente() {
+    this.clientes.pop();
   }
 
 }
